1a34b691ede24064d391803919f4fb08e6387735
Matthew Logan <loganmatthewdev@gmail.com>
2024-09-16 22:09:09 -0700

    Add working fetch request to base app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Content from "./components/Content/Content";
+import Settings from "./components/Settings/Settings";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [weather, setWeather] = useState<Record<string, any>>();
+  document.body.style.backgroundImage = `url('/Mowgli1.jpg')`;
 
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await fetch(
+          "https://localnewstv-todo.onrender.com/api/weather",
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify({ location: "Richmond BC" }),
+          }
+        );
+        console.log(data);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Settings />
+      <Content />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
