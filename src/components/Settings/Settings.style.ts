d31d4a50e2c706d8f0cddac5b36545bde2fd91b4
Matthew Logan <loganmatthewdev@gmail.com>
2024-09-16 22:08:17 -0700

    Create settings tab

---
+++ src/components/Settings/Settings.style.ts
@@ -0,0 +1,37 @@
+import styled from "@emotion/styled";
+
+type ContainerProps = {
+  transform: number;
+};
+export const Container = styled.div<ContainerProps>`
+  align-items: center;
+  justify-content: flex-start;
+  background-color: #00000089;
+  border: 1pt solid black;
+  display: flex;
+  flex-direction: row;
+  padding-left: 20pt;
+  height: 60pt;
+  position: absolute;
+  right: 0;
+  top: 3%;
+  border-radius: 45pt 0 0 45pt;
+  transform: translateX(${({ transform }) => transform}%);
+  transition: transform 0.5s ease-in;
+  width: 250pt;
+`;
+
+export const Button = styled.button`
+  background: none;
+  border: none;
+  border-radius: 45%;
+  height: 30;
+  margin: 0;
+  padding: 0;
+  width: 30;
+`;
+
+export const Img = styled.img`
+  height: 30pt;
+  width: 30pt;
+`;
---
+++ src/components/Settings/Settings.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { Button, Container, Img } from "./Settings.style";
+import settings from "/gear-fill.svg";
+
+const Settings = () => {
+  const BASE_TOGGLE = 77;
+  const [transform, setTransform] = useState<number>(BASE_TOGGLE);
+  const [toggle, setToggle] = useState<boolean>(false);
+  return (
+    <Container
+      transform={transform}
+      onClick={() => {
+        setToggle((toggle) => !toggle);
+        setTransform(toggle ? BASE_TOGGLE : 0);
+      }}
+      onMouseEnter={() => setTransform(toggle ? 3 : BASE_TOGGLE - 3)}
+      onMouseLeave={() => setTransform(toggle ? 0 : BASE_TOGGLE)}
+    >
+      <Button>
+        <Img src={settings} />
+      </Button>
+    </Container>
+  );
+};
+
+export default Settings;
