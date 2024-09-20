45f275a77c79554b886110093dca04643fb548e4
Matthew Logan <loganmatthewdev@gmail.com>
2024-09-19 20:48:19 -0700

    create clock element

---
+++ src/components/Clock/Clock.style.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const Text = styled.p`
+  font-size: 12pt;
+  font-weight: bold;
+  color: #fff;
+  margin: 0;
+  padding: 0;
+`;
---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+type PropTypes = {
+  tz_id: string;
+};
+const Clock = ({ tz_id }: PropTypes) => {
+  const [time, setTime] = useState(
+    new Date().toLocaleTimeString("en-US", { timeZone: tz_id })
+  );
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(
+        new Date().toLocaleTimeString("en-US", {
+          timeZone: tz_id,
+        })
+      );
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+  return time;
+};
+
+export default Clock;
