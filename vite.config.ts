889c46fb20a0a0fda10a5155a1cc663be82ab3b9
Matthew Logan <loganmatthewdev@gmail.com>
2024-09-26 18:19:46 -0700

    Setup PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/weather/",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      includeAssets: ['*/**'],
+      manifest: {
+        name: 'LocalNewsTV Weather',
+        short_name: 'weather',
+        description: 'Get current weather',
+        theme_color: '#FFF',
+        icons: [
+          {
+            src: 'Logo192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'Logo512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  base: '/weather/',
 });
